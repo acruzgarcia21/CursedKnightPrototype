a6202e3c70e869e5c3689840ae04a55aae3f22e2
acruzgarcia21 <alejandrocruzgarcia111@gmail.com>
2026-03-28 13:55:50 -0700

    Created Card Prefab, Fixed Hand Manager, Added Deck Manager

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -9,11 +9,13 @@ public class HandManager : MonoBehaviour
     // Root of the Hand Position
     public Transform handTransform;
     // Determines how much our hand will be spread out
-    public float fanSpread = 5f;
+    public float fanSpread = 7.5f;
     // Hold a list of card objects in player's hand
     public List<GameObject> cardsInHand = new List<GameObject>();
+    public float cardSpacing = 100f;
+    public float verticalSpacing = 100f;
     
-    void Start()
+    private void Start()
     {
         AddCardToHand();
         AddCardToHand();
@@ -22,7 +24,12 @@ public class HandManager : MonoBehaviour
         AddCardToHand();
     }
 
-    public void AddCardToHand()
+    public void Update()
+    {
+        UpdateHandVisuals();
+    }
+
+    private void AddCardToHand()
     {
         // Instantiate card
         // 1. GameObject
@@ -35,14 +42,32 @@ public class HandManager : MonoBehaviour
         UpdateHandVisuals();
     }
 
-    public void UpdateHandVisuals()
+    private void UpdateHandVisuals()
     {
         var cardCount = cardsInHand.Count;
+
+        // Error handling for 1 card in hand
+        if (cardCount == 1)
+        {
+            cardsInHand[0].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            cardsInHand[0].transform.localPosition = new Vector3(0f, 0f, 0f);
+            return;
+        }
+        
         for (var i = 0; i < cardCount; i++)
         {
             // Goes through every single card and goes through each rotation
             var rotationAngle = (fanSpread * (i - (cardCount - 1) / 2f));
             cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+
+            // Helps cards visually offset in both vertical and horizontal so that cards are not stacked on each other
+            var horizontalOffset = (cardSpacing * (i - (cardCount - 1) / 2f));
+            // Normalize card position between -1 and 1
+            var normalizedPosition = (2f * i / (cardCount - 1f) - 1f);
+            var verticalOffset = verticalSpacing * (1 - normalizedPosition * normalizedPosition);
+            
+            // Set card positions
+            cardsInHand[i].transform.localPosition = new Vector3(horizontalOffset, verticalOffset, 0f);
         }
     }
 }
